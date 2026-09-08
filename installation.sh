4e599edf79be8e704cd02ceb77d233fd49432801
pydiraju <pydiraju.tech@gmail.com>
2026-09-08 14:17:52 +0530

    first commit

--- installation.sh
+++ installation.sh
@@ -5,4 +5,12 @@ USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
     echo "run this script with root user"
     exit 1
+fi  
+
+  echo "installing nginx"
+if [ $? -ne 0 ]; then
+   echo "installing nginx is failure"
+   exit 1
+else 
+    echo "installing nginx is success"
 fi    
