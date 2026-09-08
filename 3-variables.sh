71bd13aced71ba74f4364fd9861990faadc5e09c
pydiraju <pydiraju.tech@gmail.com>
2026-09-08 11:20:54 +0530

    first commit

---
+++ 3-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=sravani
+PERSON2=raju
+
+echo $PERSON1:: hai $PERSON2 . how are you
+echo $PERSON2:: hai $PERSON1 . iam doing good how about you 
+echo $PERSON1:: iam learning devops and aws 
+echo $PERSON2:: ho nice $PERSON1
