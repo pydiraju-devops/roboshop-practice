98723cbd5605fba96b566ad657e379699a0fe1d4
pydiraju <pydiraju.tech@gmail.com>
2026-09-08 13:42:53 +0530

    first commit

--- 1-conditions.sh
+++ 1-conditions.sh
@@ -2,10 +2,10 @@
 
 NUMBER=$1
 
-if ($NUMBER -ne 20); then
+if ( $NUMBER -gt 20 ); then
    echo "given number is: $NUMBER more than 20"
-elif ($NUMBER -eq 20); then 
+elif ( $NUMBER -eq 20 ); then 
     echo "given number is: $NUMBER equal to 20"
-else ($NUMBER -lt 20); then
+else ( $NUMBER -lt 20 ); then
     echo "given number is:$NUMBER less than 20"
 fi    
