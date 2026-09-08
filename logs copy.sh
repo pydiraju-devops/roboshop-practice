9b2210bbb528a4901223365c6f62746edb80b62e
pydiraju <pydiraju.tech@gmail.com>
2026-09-08 22:34:18 +0530

    first commit

--- logs copy.sh
+++ logs copy.sh
@@ -3,10 +3,10 @@
 USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
-R='\e[31m'
-G='\e[32m'
-Y='\e[33m'
-N='\e[0m'
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USER_ID -ne 0 ]; then
     echo -e "$R run this script with root user $N" | tee -a $LOGS_FILE
