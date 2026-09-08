7422ec3ed0132143132bfd140ba73deba439d89c
pydiraju <pydiraju.tech@gmail.com>
2026-09-08 18:35:21 +0530

    first commit

---
+++ functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "run this script with root user"
+    exit 1
+fi  
+
+VALIDATE(){
+        if [ $1 -ne 0 ]; then
+           echo "$2 is failure"
+           exit 1
+else 
+           echo "$2 is success"
+fi    
+}
+
+  echo "installing nginx"
+  dnf install nginx -y 
+  VALIDATE $? "installing nginx"
+
+  dnf install mysql -y 
+  VALIDATE $? "installing mysql"
+ 
+
+dnf install nodejs  -y 
+VALIDATE $? "installing noejs"
--- installation.sh
+++ installation.sh
@@ -23,7 +23,7 @@ if [ $? -ne 0 ]; then
    echo "installing mysql is failure"
    exit 1
 else 
-    echo "installing mysql is success"
+    echo "installing  is success"
 fi      
 
 dnf install nodejs  -y 
