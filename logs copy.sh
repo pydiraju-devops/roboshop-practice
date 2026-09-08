bf0b2026fc011b750f4485d42627a33d0065401b
pydiraju <pydiraju.tech@gmail.com>
2026-09-08 22:28:42 +0530

    first commit

---
+++ logs copy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+
+if [ $USER_ID -ne 0 ]; then
+    echo -e "$R run this script with root user $N" | tee -a $LOGS_FILE
+    exit 1
+fi  
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+        if [ $1 -ne 0 ]; then
+           echo -e "$2 is $R failure $N" | tee -a $LOGS_FILE
+           exit 1
+        else 
+           echo "$2 is $G success $N" | tee -a $LOGS_FILE
+fi
+
+}
+ 
+for package in $@ ###parameter should pass here 
+do 
+  dnf list installed $package &>>LOGS_FILE
+  if [ $? -ne 0 ]; then
+     echo  "$package not installed , installing now "
+     dnf install $package -y &>>LOGS_FILE
+     VALIDATE $? "$package installtion"
+    else
+       echo "$package $G already installed  , $Y skipping $N"
+    fi 
+done
