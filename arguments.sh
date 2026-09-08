06032473abd9fef3d842ad09d761be95b64fa928
pydiraju <pydiraju.tech@gmail.com>
2026-09-08 11:19:53 +0530

    first commit

--- arguments.sh
+++ arguments.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo raju :: hai sai . how are you
-echo sai :: hai raju . iam doing good how about you 
-echo raju :: iam learning devops and aws 
-echo sai :: ho nice raju
+PERSON1=raju
+PERSON2=sai
+
+echo $PERSON1:: hai $PERSON2 . how are you
+echo $PERSON2:: hai $PERSON1 . iam doing good how about you 
+echo $PERSON1:: iam learning devops and aws 
+echo $PERSON2:: ho nice $PERSON1
