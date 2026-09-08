fa4dd52e06859e13d1dc449d4de840524acb0ac4
pydiraju <pydiraju.tech@gmail.com>
2026-09-08 11:35:39 +0530

    first commit

---
+++ 4-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo $PERSON1:: hai $PERSON2 . how are you
+echo $PERSON2:: hai $PERSON1 . iam doing good how about you 
+echo $PERSON1:: iam learning devops and aws 
+echo $PERSON2:: ho nice $PERSON1
