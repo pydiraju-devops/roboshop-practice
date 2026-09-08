d70ec07d2774328c24c31c3078e46a9256f3d4b7
pydiraju <pydiraju.tech@gmail.com>
2026-09-08 12:26:11 +0530

    first commit

--- 7-variables.sh
+++ 7-variables.sh
@@ -6,6 +6,6 @@ echo "script start time $START_TIME"
 
 END_TIME=$(date +%S)
 
-echo "script total time is $TOTAL_TIME"
-
 TOTAL_TIME=$(($START_TIME-$END_TIME))
+
+echo "script total time is $TOTAL_TIME"
